6acf8c8feaa5ee149537077611d4d23b937c01c4
Mauricio Cabrera <mauriciocabrera375@gmail.com>
2023-05-18 18:21:38 -0700

    ninja 10

---
+++ 152/n10ho1.sql
@@ -0,0 +1,8 @@
+SELECT
+  pid
+  , pfirstname
+  , plastname
+FROM
+  people
+WHERE
+  pid = 18;
---
+++ 154/n10ho3.sql
@@ -0,0 +1,14 @@
+SELECT
+  pfirstname
+  , plastname
+  , pstate
+  , pdob
+FROM
+  people
+WHERE
+  (
+    pfirstname LIKE '%e%'
+    OR plastname LIKE '%e%'
+  )
+  AND pstate = 'CA'
+  AND pdob > '1999-12-31';
