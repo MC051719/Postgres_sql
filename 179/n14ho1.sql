7abd451da5666162b16884c492694f0d13c0fa77
Mauricio Cabrera <mauriciocabrera375@gmail.com>
2023-05-19 22:54:20 -0700

    ninja 14

---
+++ 179/n14ho1.sql
@@ -0,0 +1,25 @@
+SELECT
+  pfirstname
+  , plastname
+  , ppoints
+FROM
+  people
+WHERE
+  ppoints > (
+    SELECT
+      AVG(ppoints)
+    FROM
+      people
+  );
+
+SELECT
+  COUNT(*)
+FROM
+  people
+WHERE
+  ppoints > (
+    SELECT
+      AVG(ppoints)
+    FROM
+      people
+  );
